f210e6154b0f0b526987b361f57a124cb5cd5f61
Bilal Habeş Akçam <121240992+BilalAkcam@users.noreply.github.com>
2023-03-04 10:11:52 +0300

    Update uart_driver.c

--- 9-UART_RX_Intrerrupt_Command/Core/Src/uart_driver.c
+++ 9-UART_RX_Intrerrupt_Command/Core/Src/uart_driver.c
@@ -17,10 +17,7 @@ static void UART_Error_Handler(void);
 
 
 /*  
-  UART_Init()
-  UART'i proje specifikasyonunda belirtilen hiz ve
-  ayarlarla baslatir. 
-  RX kesmelerini aktif hale getirir.
+  UART_Init() belirtilen hiz ve ayarlarla UART'ı baslatir ve RX kesmelerini aktif hale getirir.
 
 */
 void UART_Init(void)
@@ -28,15 +25,10 @@ void UART_Init(void)
 	
 GPIO_InitTypeDef  GPIO_InitStruct;
   
-  /*##-1- Enable peripherals and GPIO Clocks #################################*/
-  /* Enable GPIO TX/RX clock */  
-	__HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
-  /* Enable USARTx clock */
   __HAL_RCC_USART2_CLK_ENABLE(); 
   
-  /*##-2- Configure peripheral GPIO ##########################################*/  
-  /* UART TX GPIO pin configuration  */
 	
 	
   GPIO_InitStruct.Pin       = GPIO_PIN_2;
@@ -57,14 +49,7 @@ GPIO_InitTypeDef  GPIO_InitStruct;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
     
 
-/*##-3- Configure the UART peripheral ######################################*/
-  /* Put the USART peripheral in the Asynchronous mode (UART Mode) */
-  /* UART configured as follows:
-      - Word Length = 8 Bits
-      - Stop Bit = One Stop bit
-      - Parity = None
-      - BaudRate = 9600 baud
-      - Hardware flow control disabled (RTS and CTS signals) */
+
   UartHandle.Instance        = USART2;
 	
   UartHandle.Init.BaudRate   = 9600;
@@ -78,15 +63,13 @@ GPIO_InitTypeDef  GPIO_InitStruct;
 	
   if(HAL_UART_Init(&UartHandle) != HAL_OK)
   {
-		//ERROR
+		
     UART_Error_Handler();
   }
 	
-/* 4- Enable UART Receive Data Register Not Empty */
    SET_BIT(USART2->CR1, USART_CR1_RXNEIE);
 
 	
-	/* 5 - Enable UART Interrupt in NVIC */
 	
   HAL_NVIC_SetPriority(USART2_IRQn, 0, 1);
   HAL_NVIC_EnableIRQ(USART2_IRQn);
@@ -95,11 +78,7 @@ GPIO_InitTypeDef  GPIO_InitStruct;
 
 
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @param  None
-  * @retval None
-  */
+
 static void UART_Error_Handler(void)
 {
      while(1)
@@ -109,11 +88,7 @@ static void UART_Error_Handler(void)
 }
 
 
-/**
-  * @brief  This function handles UART interrupt request.  
-  * @param  None
-  * @retval None
-  */
+
 void USART2_IRQHandler(void) {
 	uint32_t isrflags = USART2->ISR;
 	uint32_t control_reg1 = USART2->CR1;
@@ -128,7 +103,6 @@ void USART2_IRQHandler(void) {
 		return;
 	}
 
-	/* UART in mode Transmitter */
 	if (((isrflags & USART_ISR_TXE) != RESET) && ((control_reg1 & USART_CR1_TXEIE) != RESET)) {
 
 		return;
