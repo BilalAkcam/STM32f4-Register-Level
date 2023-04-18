fd68c5aae2a7d76b98880ca5c7e0648b51e1b180
Bilal Habeş Akçam <bh.akcam@gmail.com>
2023-04-19 02:43:11 +0300

    Project Update

---
+++ 14-Timer-Driver/Core/Inc/Led.h
@@ -0,0 +1,21 @@
+/*
+ * Led.h
+ *
+ *  Created on: Jan 19, 2023
+ *      Author: Bilal Habes Akcam
+ */
+
+
+#ifndef LEDDRIVER_INC_LED_H_
+#define LEDDRIVER_INC_LED_H_
+
+#include "stm32f4xx_hal.h"
+
+void ledInit(void);
+void ledOn(void);
+void ledOff(void);
+
+
+
+
+#endif /* LEDDRIVER_INC_LED_H_ */
